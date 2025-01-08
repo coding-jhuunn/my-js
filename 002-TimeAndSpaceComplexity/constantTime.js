03c2538135a76e18bb4124151c15a73b541d1f90
coding-jhuunn <codingatjhunn@gmail.com>
2025-01-08 20:06:28 +0800

    add constantTime

---
+++ 002-TimeAndSpaceComplexity/constantTime.js
@@ -0,0 +1,6 @@
+function performAdd(n) {
+  let sum = (n * (n + 1)) / 2;
+  return sum;
+}
+// the input size doesn't matter, it always be completed the exact amout of steps
+// in the example it should be 0(3), but it is in smaller operations it doesn't matter
