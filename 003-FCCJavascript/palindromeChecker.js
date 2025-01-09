aede0d9b3e2f4bd58e6cc9a98d1bfb440920025c
coding-jhuunn <codingatjhunn@gmail.com>
2025-01-09 11:11:47 +0800

    add FCC-JS folder & palindromeChecker

---
+++ 003-FCCJavascript/palindromeChecker.js
@@ -0,0 +1,17 @@
+function palindrome(str) {
+  let replaceStr = str.replace(/[^a-zA-Z0-9]/g, "").toLowerCase();
+
+  result = replaceStr.split("").reverse().join("");
+  if (result === replaceStr) {
+    return true;
+  }
+  return false;
+}
+
+console.log(palindrome("eye"));
+console.log(palindrome("A man, a plan, a canal. Panama"));
+console.log(palindrome("1234"));
+console.log(palindrome("_eye"));
+console.log(palindrome("race car"));
+console.log(palindrome("five|_/|four"));
+console.log(palindrome("My age is 0, 0 si ega ym."));
