26f79ed3dcd5c8fd591b86c2a0d31c201e93877a
coding-jhuunn <codingatjhunn@gmail.com>
2025-01-16 16:13:15 +0800

    fixed

--- 003-FCCJavascript/telephoneNumber.js
+++ 003-FCCJavascript/telephoneNumber.js
@@ -1,5 +1,6 @@
 function telephoneCheck(str) {
-  let regExp = /(1\s?)?(\d{3}|\(\d{3}\))[\-\s]?\d{3}[\-\s]?\d{4}/g;
+  let regExp = /^(1\s?)?(\d{3}|\(\d{3}\))[\-\s]?\d{3}[\-\s]?\d{4}$/g;
+  return regExp.test(str);
 }
 
 telephoneCheck("555-555-5555");
