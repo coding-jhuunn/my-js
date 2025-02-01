37113287fcf8cb2bfa9cbd4f8eb12cb130c8deea
coding-jhuunn <codingatjhunn@gmail.com>
2025-02-01 20:13:54 +0800

    arr files

--- 005-FeDevLib/RandomQuoteMachine/scripts.js
+++ 005-FeDevLib/RandomQuoteMachine/scripts.js
@@ -1,7 +1,24 @@
-fetch("https://type.fit/api/quotes")
-  .then(function (response) {
-    return response.json();
-  })
-  .then(function (data) {
-    console.log(data);
-  });
+// const api_url = "https://quotes-api-self.vercel.app/quote";
+
+// async function getapi(url) {
+//   const response = await fetch(url);
+//   var data = await response.json();
+//   return data;
+// }
+
+// let value = getapi(api_url);
+
+// console.log(value);
+// "https://quotes-api-self.vercel.app/quote"
+// api to be used
+const settings = {
+  async: true,
+  crossDomain: true,
+  url: "https://quotes-api-self.vercel.app/quote",
+  method: "GET",
+};
+
+$.ajax(settings).done(function (response) {
+  const data = JSON.parse(response);
+  console.log(data);
+});
