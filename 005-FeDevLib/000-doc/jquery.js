cafb587c92e3590a2adbe0293fa365d1534da0ab
coding-jhuunn <codingatjhunn@gmail.com>
2025-02-02 21:37:23 +0800

    add docu to jquery section

--- 005-FeDevLib/000-doc/jquery.js
+++ 005-FeDevLib/000-doc/jquery.js
@@ -3,3 +3,10 @@ $("p").hide();
 // $ sign to define/access jQuery
 // (selector) to "query (or find)" HTML elements
 // jQuery action() to be performed on the element(s)
+// $(this).hide() - hides the current element.
+
+// $("p").hide() - hides all <p> elements.
+
+// $(".test").hide() - hides all elements with class="test".
+
+// $("#test").hide() - hides the element with id="test".
